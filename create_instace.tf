dd27318baac0f4d0a27d3dd1e5cd83264dc4db12
Ubuntu <ubuntu@ip-172-31-26-225.us-east-2.compute.internal>
2022-10-19 05:46:01 +0000

    files are aadded

--- create_instace.tf
+++ create_instace.tf
@@ -1,10 +1,34 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAVUG65NVQB6WWGICT"
-  secret_key = "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"
+  access_key = "AKIAVUG65NVQKV4MEAZ3"
+  secret_key = "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"
 }
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAVUG65NVQKV4MEAZ3"
+  secret_key = "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"
+  alias = "useast1"
+}
+
 
 resource "aws_instance" "us-east2" {
   ami           = "ami-0a59f0e26c55590e9"
   instance_type = "t2.micro"
+}
+
+resource "aws_instance" "us-east1" {
+  ami           = "ami-0ee23bfc74a881de5"
+  instance_type = "t2.micro"
+provider = aws.useast1
+}
+
+
+resource "aws_s3_bucket" "firstbucket" {
+  bucket = "my-tf-test-bucket-kdjaskjdhdhhd"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev-env"
+  }
 }
